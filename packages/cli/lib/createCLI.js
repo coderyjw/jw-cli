61021b394dd44ecce75aa90dc84c57f1e64071c4
叶继伟 <yejw6@asiainfo.com>
2023-01-30 22:45:58 +0800

    refactor: createCLI.js 逻辑抽离

---
+++ packages/cli/lib/createCLI.js
@@ -0,0 +1,35 @@
+import fse from "fs-extra";
+import chalk from "chalk";
+import path from "node:path";
+import { dirname } from "dirname-filename-esm";
+import { log } from "@yejiwei/utils";
+
+const __dirname = dirname(import.meta);
+const pkgPath = path.resolve(__dirname, "../package.json");
+const pkg = fse.readJsonSync(pkgPath);
+const LOWEST_NODE_VERSION = "17.0.0";
+
+const checkNodeVersion = () => {
+  log.verbose("node version", process.version);
+  if (!semver.gte(process.version, LOWEST_NODE_VERSION)) {
+    throw new Error(
+      chalk.red(`jw-cli 需要安装 ${LOWEST_NODE_VERSION} 以上版本的 Node.js`)
+    );
+  }
+};
+
+const preAction = () => {
+  // 检查 Node 版本
+  checkNodeVersion();
+};
+
+export default function createCLI(program) {
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .usage("<command> [options]")
+    .version(pkg.version, "-v, --version", "输出当前版本号")
+    .option("-d, --debug", "是否开启调试模式", false)
+    .helpOption("-h, --help", "命令显示帮助")
+    .addHelpCommand("help [command]", "命令显示帮助")
+    .hook("preAction", preAction);
+}
--- packages/cli/lib/index.js
+++ packages/cli/lib/index.js
@@ -1,30 +1,7 @@
-import path from "node:path";
 import { program } from "commander";
-import semver from "semver";
-import chalk from "chalk";
-import fse from "fs-extra";
 import { log, isDebug } from "@yejiwei/utils";
 import createInitCommand from "@yejiwei/init";
-import { dirname } from "dirname-filename-esm";
-
-const __dirname = dirname(import.meta);
-const pkgPath = path.resolve(__dirname, "../package.json");
-const pkg = fse.readJsonSync(pkgPath);
-const LOWEST_NODE_VERSION = "17.0.0";
-
-const checkNodeVersion = () => {
-  log.verbose("node version", process.version);
-  if (!semver.gte(process.version, LOWEST_NODE_VERSION)) {
-    throw new Error(
-      chalk.red(`jw-cli 需要安装 ${LOWEST_NODE_VERSION} 以上版本的 Node.js`)
-    );
-  }
-};
-
-const preAction = () => {
-  // 检查 Node 版本
-  checkNodeVersion();
-};
+import createCLI from "./createCLI.js";
 
 process.on("uncaughtException", (e) => {
   if (isDebug()) {
@@ -35,14 +12,7 @@ process.on("uncaughtException", (e) => {
 });
 
 export default function (args) {
-  program
-    .name(Object.keys(pkg.bin)[0])
-    .usage("<command> [options]")
-    .version(pkg.version, "-v, --version", "输出当前版本号")
-    .option("-d, --debug", "是否开启调试模式", false)
-    .helpOption("-h, --help", "命令显示帮助")
-    .addHelpCommand("help [command]", "命令显示帮助")
-    .hook("preAction", preAction);
+  createCLI(program);
 
   createInitCommand(program);
 
