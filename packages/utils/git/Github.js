2d42455bfd5aede7bb5c0c373b616ea997db0135
叶继伟 <yejw6@asiainfo.com>
2023-02-10 16:11:22 +0800

    feat: github 仓库创建 API 接入

--- packages/utils/git/Github.js
+++ packages/utils/git/Github.js
@@ -1,6 +1,8 @@
 import { GitServer } from "./GitServer.js";
 import axios from "axios";
 
+import { log } from "../lib/index.js";
+
 // token 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
 const BASE_URL = "https://api.github.com";
 export default class GitHub extends GitServer {
@@ -41,7 +43,17 @@ export default class GitHub extends GitServer {
     });
   }
 
-  post() {}
+  post(url, data, headers) {
+    return this.service({
+      url,
+      data,
+      params: {
+        access_token: this.token,
+      },
+      method: "post",
+      headers,
+    });
+  }
 
   searchRepositories(params) {
     return this.get("search/repositories", params);
@@ -65,5 +77,39 @@ export default class GitHub extends GitServer {
 
   getOrg() {
     return this.get("/user/orgs");
+  }
+
+  getRepo(owner, repo) {
+    return this.get(
+      `/repos/${owner}/${repo}`,
+      {},
+      {
+        accept: "application/vnd.github+json",
+      }
+    ).catch((err) => {
+      return null;
+    });
+  }
+
+  async createRepo(name) {
+    // 检查远程仓库是否存在，如果存在，则跳过创建
+    let repo = await this.getRepo(this.login, name);
+    if (!repo) {
+      log.info("仓库不存在，开始创建");
+      if (this.own === "user") {
+        repo = await this.post("/user/repos", { name }, { accept: "application/vnd.github+json" });
+      } else if (this.own === "org") {
+        const url = "orgs/" + this.login + "/repos";
+        repo = await this.post(url, { name }, { accept: "application/vnd.github+json" });
+      }
+
+      if (repo?.id) {
+        log.success("仓库创建成功");
+      }
+    } else {
+      log.info("仓库存在，直接返回");
+    }
+    log.success("仓库地址", repo.html_url);
+    return repo;
   }
 }
